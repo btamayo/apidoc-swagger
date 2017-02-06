e9bc00f545bd7efee01e038a04282c81af81447a
Bianca Tamayo <bancie@gmail.com>
2017-02-05 18:56:37 -0800

    [WIP] Cleanup, reorg
    * Reorganized functions
    * Code cleanup
    * Progress on POST params
    * Comments for clarity & my own understanding

--- test/sample_api_raw.py
+++ test/sample_api_raw.py
@@ -18,3 +18,27 @@ def getPet():
     """
 
     return
+
+@route("/pets/:petId", method='post')
+def postPet():
+    """
+    @api {post} /pets/:petId /pets/:petId
+    @apiName postPet
+    @apiGroup pet
+    @apiVersion 0.2.0
+    @apiDescription Adds a new pet to the store
+
+    @apiParam {String} petId the id of the pet
+
+    @apiParam {String} appKey Application Key to access the API
+    @apiParam {String} appSecret Application Key Secret to access the API
+
+    @apiParam {String} name pet's name
+    @apiParam {String} [nickname] optional pet's nickname
+
+    @apiSuccess {object} pet pet
+    @apiSuccessExample {json} Example 200 Response
+      TODO
+    """
+
+    return
